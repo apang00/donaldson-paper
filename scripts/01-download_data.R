c7ffb9b60652ec4cbf2aeb919946cea9042589bb
Alex <alex.pang@jci.com>
2024-01-22 19:59:39 -0500

    completed download_data.R and added raw data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,37 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from two datasets from OpenDataToronto
+# Author: Yi Fei Pang
+# Date: 2024-01-23
+# Contact: yifei.pang@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
 
 
-#### Workspace setup ####
+#### Workplace Setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(dplyr)
+library(readr)
 
-#### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+#### Donload Data ####
 
+# Daily shelter visitor count
+package <- show_package("21c83b32-d5a8-4106-a54f-010dbe49f6f2")
+
+# get all resources for this package
+resources <- list_package_resources("21c83b32-d5a8-4106-a54f-010dbe49f6f2")
+
+# identify datastore resources; by default, Toronto Open Data sets datastore 
+# resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+
+# load the first datastore resource as a sample
+shelter_count <- filter(datastore_resources, row_number()==1) %>% get_resource()
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(x = shelter_count, file = "shelter_count.csv")
+
+#### Read Data ####
+readr::read_csv("inputs/data/shelter_count.csv")
 
          
